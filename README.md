2df774c5d9c9ef727b16b8f799ff597c392918f3
Endashaw Demsis <Endashaw Demsis@DESKTOP-824A2R3>
2022-07-01 08:48:20 +0300

    Initalize

---
+++ README.md
@@ -0,0 +1,47 @@
+[Demo](http://170.187.203.187/login)
+
+# open-survey
+
+open-survey is a simple survey collection tool that allows the user to create dynamic forms which is mostly used by companies, researchers or Individual to capture a given set of data without creating website or application.
+
+This repo is the backend, for the frontend go to [here](https://github.com/paulreeddev/open-survey)
+
+This Project is made possible by
+
+<table>
+<tr>
+        <td>
+            <a href="https://www.linode.com/"><img src="https://www.linode.com/wp-content/uploads/2021/01/Linode-Logo-Black.svg" /></a>
+        </td>
+        <td>
+            <a href="https://hashnode.com/"><img src="https://cdn.hashnode.com/res/hashnode/image/upload/v1592751328987/VzrtgcQNF.jpeg" /></a>
+        </td>
+    </tr>
+</table>
+
+## Requirements
+
+You need to have PHP version **8.1** or above. Node.js version **12.0** or above.
+
+## Installation
+
+#### Backend
+
+1. Clone the project
+2. Go to the project root directory
+3. Run `composer install`
+4. Create database
+5. Copy `.env.example` into `.env` file and adjust parameters
+6. Run `php artisan serve` to start the project at http://localhost:8000
+
+#### Frontend
+
+1. Navigate to `vue` folder using terminal
+2. Run `npm install` to install vue.js project dependencies
+3. Copy `vue/.env.example` into `vue/.env` and specify API URL
+4. Start frontend by running `npm run dev`
+5. Open http://localhost:3000
+
+## License
+
+The project is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ app/Http/Controllers/SurveyController.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSurveyAnswerRequest;
+use App\Http\Resources\SurveyResource;
+use App\Models\Survey;
+use App\Http\Requests\StoreSurveyRequest;
+use App\Http\Requests\UpdateSurveyRequest;
+use App\Models\SurveyAnswer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+
+class SurveyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $user = $request->user();
+
+        return SurveyResource::collection(Survey::where('user_id', $user->id)->orderBy('created_at', 'DESC')->paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \App\Http\Requests\StoreSurveyRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSurveyRequest $request)
+    {
+        $data = $request->validated();
+
+        // Check if image was given and save on local file system
+        if (isset($data['image'])) {
+            $relativePath  = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+        }
+
+        $survey = Survey::create($data);
+
+        return new SurveyResource($survey);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Survey $survey
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Survey $survey, Request $request)
+    {
+        $user = $request->user();
+        if ($user->id !== $survey->user_id) {
+            return abort(403, 'Unauthorized action.');
+        }
+
+        return new SurveyResource($survey);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Survey $survey
+     * @return \Illuminate\Http\Response
+     */
+    public function showForGuest(Survey $survey)
+    {
+        if (!$survey->status) {
+            return response("", 404);
+        }
+
+        $currentDate = new \DateTime();
+        $expireDate = new \DateTime($survey->expire_date);
+        if ($currentDate > $expireDate) {
+            return response("", 404);
+        }
+
+        return new SurveyResource($survey);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \App\Http\Requests\UpdateSurveyRequest $request
+     * @param \App\Models\Survey                     $survey
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSurveyRequest $request, Survey $survey)
+    {
+        $data = $request->validated();
+
+        // Check if image was given and save on local file system
+        if (isset($data['image'])) {
+            $relativePath = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+
+            // If there is an old image, delete it
+            if ($survey->image) {
+                $absolutePath = public_path($survey->image);
+                File::delete($absolutePath);
+            }
+        }
+
+        // Update survey in the database
+        $survey->update($data);
+
+        return new SurveyResource($survey);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Survey $survey
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Survey $survey, Request $request)
+    {
+        $user = $request->user();
+        if ($user->id !== $survey->user_id) {
+            return abort(403, 'Unauthorized action.');
+        }
+
+        $survey->delete();
+
+        // If there is an old image, delete it
+        if ($survey->image) {
+            $absolutePath = public_path($survey->image);
+            File::delete($absolutePath);
+        }
+
+        return response('', 204);
+    }
+
+    public function storeAnswer(StoreSurveyAnswerRequest $request, Survey $survey)
+    {
+        $validated = $request->validated();
+//        var_dump($validated, $survey);
+
+        $surveyAnswer = SurveyAnswer::create([
+            'survey_id' => $survey->id,
+            'survey_submitted_answer'=>$validated['answer'],
+            'start_date' => date('Y-m-d H:i:s'),
+            'end_date' => date('Y-m-d H:i:s'),
+        ]);
+
+        return response("", 201);
+
+    }
+
+    /**
+     * Save image in local file system and return saved image path
+     *
+     * @param $image
+     * @throws \Exception
+     
+     */
+    private function saveImage($image)
+    {
+        // Check if image is valid base64 string
+        if (preg_match('/^data:image\/(\w+);base64,/', $image, $type)) {
+            // Take out the base64 encoded text without mime type
+            $image = substr($image, strpos($image, ',') + 1);
+            // Get file extension
+            $type = strtolower($type[1]); // jpg, png, gif
+
+            // Check if file is an image
+            if (!in_array($type, ['jpg', 'jpeg', 'gif', 'png'])) {
+                throw new \Exception('invalid image type');
+            }
+            $image = str_replace(' ', '+', $image);
+            $image = base64_decode($image);
+
+            if ($image === false) {
+                throw new \Exception('base64_decode failed');
+            }
+        } else {
+            throw new \Exception('did not match data URI with image data');
+        }
+
+        $dir = 'images/';
+        $file = Str::random() . '.' . $type;
+        $absolutePath = public_path($dir);
+        $relativePath = $dir . $file;
+        if (!File::exists($absolutePath)) {
+            File::makeDirectory($absolutePath, 0755, true);
+        }
+        file_put_contents($relativePath, $image);
+
+        return $relativePath;
+    }
+
+}
